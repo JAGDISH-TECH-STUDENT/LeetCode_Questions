a7d62e3bf53152988bf582f84077cf9c50258ee6
JAGDISH <js8724285@gmail.com>
2026-07-11 19:56:53 +0530

    Time: 0 ms (100%), Space: 42.9 MB (18.46%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,29 +1,11 @@
-import java.util.*;
-
 class Solution {
     public int rob(int[] nums) {
-        int n = nums.length;
-        // dp[i][free] = max profit from index i with free status
-        int[][] dp = new int[n][2];
-        for (int[] row : dp) Arrays.fill(row, -1);
-        return fun(nums, n, 0, 1, dp); // start at index 0, free = 1
-    }
-
-    private int fun(int[] a, int n, int i, int free, int[][] dp) {
-        if (i == n) return 0; // base case
-
-        if (dp[i][free] != -1) return dp[i][free];
-
-        if (free == 0) {
-            // अगर पिछला घर rob किया है, तो current घर skip करना पड़ेगा
-            return dp[i][free] = fun(a, n, i + 1, 1, dp);
+        int prev1 = 0, prev2 = 0;
+        for (int num : nums) {
+            int temp = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = temp;
         }
-
-        // Option 1: rob current house
-        int c1 = a[i] + fun(a, n, i + 1, 0, dp);
-        // Option 2: skip current house
-        int c2 = fun(a, n, i + 1, 1, dp);
-
-        return dp[i][free] = Math.max(c1, c2);
+        return prev1;
     }
 }
