ca5d79c6e72ef7148bb5ec0bdeaa3688393e104a
JAGDISH <js8724285@gmail.com>
2026-07-21 13:51:14 +0530

    Time: 3 ms (55.28%), Space: 45.9 MB (98.31%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,45 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        // Check rows
+        for (int i = 0; i < 9; i++) {
+            Set<Character> rowSet = new HashSet<>();
+            for (int j = 0; j < 9; j++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    if (rowSet.contains(c)) return false;
+                    rowSet.add(c);
+                }
+            }
+        }
+
+        // Check columns
+        for (int j = 0; j < 9; j++) {
+            Set<Character> colSet = new HashSet<>();
+            for (int i = 0; i < 9; i++) {
+                char c = board[i][j];
+                if (c != '.') {
+                    if (colSet.contains(c)) return false;
+                    colSet.add(c);
+                }
+            }
+        }
+
+        // Check 3x3 sub-boxes
+        for (int boxRow = 0; boxRow < 3; boxRow++) {
+            for (int boxCol = 0; boxCol < 3; boxCol++) {
+                Set<Character> boxSet = new HashSet<>();
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        char c = board[boxRow * 3 + i][boxCol * 3 + j];
+                        if (c != '.') {
+                            if (boxSet.contains(c)) return false;
+                            boxSet.add(c);
+                        }
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+}
