341730e260050adc054744cf81ec9f331153282a
JAGDISH <js8724285@gmail.com>
2026-08-01 15:32:12 +0530

    Time: 72 ms (15.24%), Space: 43.2 MB (5.44%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -4,13 +4,7 @@ class Solution {
         for(int i=0;i<dp.length;i++){
             Arrays.fill(dp[i],-1);
         }
-        int totalSum=0;
-        for(int i=0;i<nums.length;i++){
-            totalSum+=nums[i];
-        }
-        int player1=solve(0,nums.length-1,nums,dp);
-        int player2=totalSum-player1;
-        return player1>=player2;
+        return solve(0,nums.length-1,nums,dp)>=0;
 
     }
     public int solve(int i,int j,int[]nums,int[][] dp){
@@ -20,13 +14,13 @@ class Solution {
         if(i==j){
             return nums[i];
         }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
+        // if(dp[i][j]!=-1){
+        //     return dp[i][j];
+        // }
 
-        int take_i=nums[i]+Math.min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
-        int take_j=nums[j]+Math.min(solve(i+1,j-1,nums,dp),solve(i,j-2,nums,dp));
-        return dp[i][j]=Math.max(take_i,take_j);
+        int take_i=nums[i]-solve(i+1,j,nums,dp);
+        int take_j=nums[j]-solve(i,j-1,nums,dp);
+        return Math.max(take_i,take_j);
 
     }
 }
