d2f5e4d8a66286f2a781b61bffd9b6442c673c89
JAGDISH <js8724285@gmail.com>
2026-08-24 19:30:29 +0530

    3 ms (100%) 92.2 MB (29.36%) 1872-stone-game-viii .java

---
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int stoneGameVIII(int[] stones) {
+        int n = stones.length;
+        int[] pre = new int[n];
+        pre[0] = stones[0];
+        for (int i = 1; i < n; i++) {
+            pre[i] = pre[i - 1] + stones[i];
+        }
+        
+        int dp = pre[n - 1];
+        for (int i = n - 2; i >= 1; i--) {
+            dp = Math.max(dp, pre[i] - dp);
+        }
+        
+        return dp;
+    }
+}
