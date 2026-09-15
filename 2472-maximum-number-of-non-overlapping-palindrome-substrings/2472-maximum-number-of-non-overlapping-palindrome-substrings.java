ae6949c35ab82436bf23c8dd6d8bbba01f904150
JAGDISH <js8724285@gmail.com>
2026-09-15 19:57:56 +0530

    46 ms (49.25%) 61.9 MB (40.7%) 2472-maximum-number-of-non-overlapping-palindrome-substrings .java

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxPalindromes(String s, int k) {
+        int n = s.length();
+        boolean[][] isPal = new boolean[n][n];
+
+        // Precompute palindromes
+        for (int center = 0; center < n; center++) {
+            expand(s, center, center, isPal);     // odd length
+            expand(s, center, center + 1, isPal); // even length
+        }
+
+        int[] dp = new int[n + 1];
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = dp[i + 1]; // skip current
+            for (int j = i + k - 1; j < n; j++) {
+                if (isPal[i][j]) {
+                    dp[i] = Math.max(dp[i], 1 + dp[j + 1]);
+                }
+            }
+        }
+        return dp[0];
+    }
+
+    private void expand(String s, int l, int r, boolean[][] isPal) {
+        int n = s.length();
+        while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
+            isPal[l][r] = true;
+            l--; r++;
+        }
+    }
+}
